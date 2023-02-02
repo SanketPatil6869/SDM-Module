6c1732aca78384e324d4bc9491445dbc109b7e89
Sanket Patil <sp1524299@gmail.com>
2023-02-02 15:27:52 +0530

    All Done

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class BubbleSort {
 
     public static void main(String[] args) {
@@ -9,24 +11,53 @@ class BubbleSort {
         arr[3] = 11;
         arr[4] = 7;
 
-        for (int i = 0; i < arr.length; i++) {
+        System.out.println("Enter 1 for Bubble sort & 2 for Insertion sort 3 for exit");
+        Scanner sc = new Scanner(System.in);
+        int no = sc.nextInt();
+        switch (no) {
+            case 1:
+                for (int i = 0; i < arr.length; i++) {
 
-            for (int j = 0; j < arr.length; j++) {
+                    for (int j = 0; j < arr.length; j++) {
 
-                if (arr[j] < arr[i]) {
+                        if (arr[j] < arr[i]) {
 
-                    int temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
+                            int temp = arr[j];
+                            arr[j] = arr[i];
+                            arr[i] = temp;
+                        }
+
+                    }
+
                 }
 
+                break;
+            case 2:
+                int high = arr[0];
+
+                for (int i = 0; i < arr.length; i++) {
+
+                    for (int j = 0; j < arr.length; j++) {
+
+                        if (arr[j] < high) {
+
+                            int temp = arr[j];
+                            arr[j] = high;
+                            high = temp;
+                        }
+
+                    }
+
+                }
+
+                break;
+
+        }
+        while (no == 3)
+
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i]);
             }
-
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
 
     }
 }
---
+++ Sorting.java
@@ -0,0 +1,32 @@
+class Sorting {
+
+    public static void main(String[] args) {
+
+        int arr[] = new int[5];
+        arr[0] = 5;
+        arr[1] = 15;
+        arr[2] = 2;
+        arr[3] = 11;
+        arr[4] = 7;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = 0; j < arr.length; j++) {
+
+                if (arr[j] > arr[i]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+
+            }
+
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
