f12d7fce3f5db88aba21bc61e44e47dff130fb3c
Sanket Patil <sp1524299@gmail.com>
2023-02-02 15:13:51 +0530

    Branch4 Added

--- BubbleSort.java
+++ BubbleSort.java
@@ -13,11 +13,11 @@ class BubbleSort {
 
             for (int j = 0; j < arr.length; j++) {
 
-                if (arr[i] < arr[j]) {
+                if (arr[j] < arr[i]) {
 
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
                 }
 
             }
