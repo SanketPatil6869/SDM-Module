dd30b83671e9c2ffe7ab27104ee7ffc0a2ccff82
Sanket Patil <sp1524299@gmail.com>
2023-02-02 14:41:33 +0530

    Braches committed

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,5 +2,11 @@ class BubbleSort {
 
     public static void main(String[] args) {
 
+        int arr[] = new int[] { 15, 12, 4, 8, 57, 16, 7 };
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
     }
 }
